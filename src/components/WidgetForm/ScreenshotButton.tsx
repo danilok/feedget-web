47888ce0490b6f72a166488a5c40eb952c9f1e9a
Danilo Yorinori <daniloksy@gmail.com>
2022-05-07 15:24:27 -0300

    Controle de erro mínimo e ajuste feedback loading

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -48,7 +48,7 @@ export function ScreenshotButton({
       onClick={handleTakeScreenshot}
       className='p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors'
     >
-      { isTakingScreenshot ? <Loading /> : <Camera className='w-6 h-6' />}
+      { isTakingScreenshot ? <Loading /> : <Camera className='w-6 h-6' /> }
     </button>
   );
 }
--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -28,17 +28,25 @@ export function FeedbackContentStep({
     event.preventDefault();
     setIsSendingFeedback(true);
     
-    console.log(screenshot);
-    
+    // console.log({
+    //   type: feedbackType,
+    //   comment,
+    //   screenshot
+    // });
 
-    await api.post('/feedbacks', {
-      type: feedbackType,
-      comment,
-      screenshot
-    });
-    
-    setIsSendingFeedback(false);
-    onFeedbackSent();
+    try {
+      await api.post('/feedbacks', {
+        type: feedbackType,
+        comment,
+        screenshot
+      });
+      
+      setIsSendingFeedback(false);
+      onFeedbackSent();
+    } catch (error) {
+      console.log(error);
+      setIsSendingFeedback(false);
+    }
   }
 
   return (
@@ -78,7 +86,7 @@ export function FeedbackContentStep({
           <button
             type='submit'
             disabled={comment.length === 0 || isSendingFeedback}
-            className='p-2 bg-brand-500 rounded-md border-transparent flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500'
+            className='p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500'
           >
             { isSendingFeedback ? <Loading /> : 'Enviar feedback' }
           </button>
